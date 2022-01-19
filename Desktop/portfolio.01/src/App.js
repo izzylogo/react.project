2789887c8778d7038e1e691fe04b7895c8e39b79
Omitiran Israel <omitiranisrael@gmail.com>
2022-01-19 18:56:10 +0100

    project setup

---
+++ Desktop/portfolio.01/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Portfolio tutorial</h2>
+      <h3>This is a new day</h3>
+
+    </div>
+  );
+}
+
+export default App;
